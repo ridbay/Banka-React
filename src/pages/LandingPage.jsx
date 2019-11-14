f3002302ea703e643fca446b4be1108cfdf8adf8
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-14 15:54:06 +0100

    Added links to landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -37,24 +37,10 @@ const useStyles = makeStyles(theme => ({
   heroButtons: {
     marginTop: theme.spacing(4),
   },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
+
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
+    padding: theme.spacing(35),
   },
 }));
 
