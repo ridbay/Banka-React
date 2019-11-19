80bec3cc2388f91ec8fb55a904a37df8a0ad0ac8
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-19 09:00:30 +0100

    Add Footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,26 +9,34 @@ import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
 
+import Image from '../img/bg.jpg'
 
-
-const Copyright = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://balogunridwan.com/">
-      RidBank
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// const Copyright = () => {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://balogunridwan.com/">
+//       RidBank
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
     minHeight: '20vh',
+  },
+  paperContainer: {
+    backgroundImage: `url(${Image})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    height: "100%"
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
@@ -41,7 +49,7 @@ const useStyles = makeStyles(theme => ({
   footer: {
     // backgroundColor: theme.palette.background.paper,
     backgroundColor: "#3f51b5",
-    color:"white",
+    color: "white",
     padding: theme.spacing(0, 8),
   },
 }));
@@ -50,20 +58,26 @@ const useStyles = makeStyles(theme => ({
 
 const HomePage = () => {
   const classes = useStyles();
-
+  const background = {
+    backgroundImage: `url(${Image})`,
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+  };
   return (
     <React.Fragment className={classes.root}>
-     
+
       {/* <NavBar/> */}
       <main>
+        <div style={{...background}}>
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
               Welcome to RidBank
+              
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-            RidBank is a light-weight core banking application that powers banking operations like account creation, customer deposit and withdrawals.
+              RidBank is a light-weight core banking application that powers banking operations like account creation, customer deposit and withdrawals.
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
@@ -85,7 +99,7 @@ const HomePage = () => {
             </div>
           </Container>
         </div>
-
+        </div>
       </main>
       {/* Footer */}
       {/* <footer className={classes.footer}>
