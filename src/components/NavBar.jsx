ca01bbbc5f0b1a4a2f0039448a33df184b065806
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-21 13:33:33 +0100

    Edit NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,50 +1,47 @@
 import React from 'react';
-
-import Link from '@material-ui/core/Link';
-
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
+  appBar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
   },
-
-  title: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-    links: {
-      position: 'relative',
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    }
-  }
+  toolbar: {
+      flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+      flexGrow: 1,
+  },
+  link: {
+      margin: theme.spacing(0.1, 1.5),
+  },
 }));
+
 const NavBar = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap className={classes.title}>
-            <Link color="inherit" href="/" underline='none' >Banking App</Link>
-          </Typography>
-          <Button color='inherit'><Link color="inherit" href="/" underline='none'>Home</Link></Button>
-          <Button color='inherit'><Link color="inherit" href="/signin" underline='none'>Sign In</Link></Button>
-          <Button color='inherit'><Link color="inherit" href="/signup" underline='none'>Sign Up</Link></Button>
-        </Toolbar>
-      </AppBar>
+    <React.Fragment>
+        <AppBar elevation={0} className={classes.appBar}>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                <Link color="inherit" href="/" underline='none'>Home</Link>
+      </Typography>
+                <nav>
+                    <Link variant="button" color="inherit" href="/about" className={classes.link}>About Us </Link>
+                    <Link variant="button" color="inherit" href="/contact" className={classes.link}>Contact Us</Link>
+                </nav>
+                <Button href="/signin" color="inherit" variant="outlined" className={classes.link}>Login</Button>
+                <Button href="/signup" color="inherit" variant="outlined" className={classes.link}>SignUp</Button>
+            </Toolbar>
+        </AppBar>
+    </React.Fragment>
     </div>
   )
 }
