e5fe53a2c1b31c7c0c69fa9c4dba6bcc12b9cf40
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-21 12:41:51 +0100

    Dashboard page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import Dashboard from './pages/Dashboard';
 
@@ -39,7 +40,8 @@ function App() {
     <ThemeProvider theme={theme}>
       <div className='App'>
         <CssBaseline />
-        <NavBar />
+        {/* <NavBar /> */}
+        <Header />
         <Router>
           <div className='container'>
             <Switch>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
+
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles(theme => ({
     appBar: {
         borderBottom: `1px solid ${theme.palette.divider}`,
-      },
-      toolbar: {
+    },
+    toolbar: {
         flexWrap: 'wrap',
-      },
-      toolbarTitle: {
+    },
+    toolbarTitle: {
         flexGrow: 1,
-      },
-      link: {
+    },
+    link: {
         margin: theme.spacing(1, 1.5),
-      },
+    },
 }));
 
 
@@ -58,3 +50,5 @@ export default function Header() {
                 </Toolbar>
             </AppBar>
         </React.Fragment>
+    )
+}
