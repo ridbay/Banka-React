5f91ec4e67914adec907902f737fe8c2ac0f0671
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-25 20:34:03 +0100

    Remove footer from dashboard

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-import Footer from './components/Footer';
 import Dashboard from './pages/dashboard/Dashboard';
 
 import {
@@ -51,7 +50,7 @@ function App() {
             </Switch>
           </div>
         </Router>
-        <Footer />
+
       </div>
 
     </ThemeProvider>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -133,7 +133,7 @@ export default function Dashboard() {
             Dashboard
           </Typography>
           <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
+            <Badge badgeContent={30} color="secondary">
               <NotificationsIcon />
             </Badge>
           </IconButton>
