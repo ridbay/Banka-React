848cdc10e9b7dab99c224ca2461da578fb6fd6d6
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-28 19:24:33 +0100

    Create Profile component

---
+++ src/Data/userData.js
@@ -0,0 +1,9 @@
+export const user = {
+fistName: "Ridwan",
+lastName: 'Balogun',
+phone: "08078197526",
+address: "Lagos, Nigeria",
+gender: 'male',
+email: 'balogunridwan@gmail.com',
+dob: '1989-04-21'
+}
