22985cd0543d2f413aa3ad47e224d701227eea6c
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-02 15:34:19 +0100

    Edit Transactions

--- src/components/client/Transactions.js
+++ src/components/client/Transactions.js
@@ -14,11 +14,11 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
 }
 
 const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
+  createData(0, '16 Mar, 2019', 'Elvis Presley', '3042863673', 'Savings', 312.44),
+  createData(1, '16 Mar, 2019', 'Paul McCartney', '3042863673', 'Current', 866.99),
+  createData(2, '16 Mar, 2019', 'Tom Scholz', '3042863673', 'Savings', 100.81),
+  createData(3, '16 Mar, 2019', 'Michael Jackson', '3042863673', 'Savings', 654.39),
+  createData(4, '15 Mar, 2019', 'Bruce Springsteen', '3042863673', 'Current', 212.79),
 ];
 
 function preventDefault(event) {
@@ -41,9 +41,9 @@ export default function Transactions() {
           <TableRow>
             <TableCell>Date</TableCell>
             <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Account Number</TableCell>
+            <TableCell>Account Type</TableCell>
+            <TableCell align="right">Amount</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
