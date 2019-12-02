b19c02d8c46ca9c683208a4ebd88b8afe845fcd0
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-02 13:43:11 +0100

    Create DashboardMain component and separate it from the dashboard

---
+++ src/components/DashboadMain.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import Chart from '../../src/pages/dashboard/Chart';
+import Deposits from '../../src/pages/dashboard/Deposits';
+import Transactions from '../../src/pages/dashboard/Transactions';
+
+
+
+const useStyles = makeStyles(theme => ({
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        width:'100vw',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
+
+const DashboadMain = () => {
+    const classes = useStyles();
+
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    return (
+        <div>
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Grid container spacing={3}>
+                        {/* Chart */}
+                        <Grid item xs={12} md={8} lg={9}>
+                            <Paper className={fixedHeightPaper}>
+                                <Chart />
+                            </Paper>
+                        </Grid>
+                        {/* Recent Deposits */}
+                        <Grid item xs={12} md={4} lg={3}>
+                            <Paper className={fixedHeightPaper}>
+                                <Deposits />
+                            </Paper>
+                        </Grid>
+                        {/* Recent Transactions */}
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <Transactions />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </main>
+        </div>
+    )
+}
+
+export default DashboadMain;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -10,20 +10,14 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import MenuIcon from '@material-ui/icons/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import ProfileMenu from '../../components/ProfileMenu'
+import ProfileMenu from '../../components/ProfileMenu';
+import DashboardMain from '../../components/DashboadMain';
 import { mainListItems } from './listItems';
-import Chart from './Chart';
-import Deposits from './Deposits';
-import Transactions from './Transactions';
+
 
 const drawerWidth = 240;
 
@@ -87,24 +81,7 @@ const useStyles = makeStyles(theme => ({
     },
   },
   appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
+
 }));
 
 
@@ -134,7 +111,6 @@ const Dashboard = () => {
   };
 
   
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
     <div className={classes.root}>
@@ -191,31 +167,8 @@ const Dashboard = () => {
 
 
       </Drawer>
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                <Chart />
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
-            {/* Recent Transactions */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                <Transactions />
-              </Paper>
-            </Grid>
-          </Grid>
-        </Container>
-      </main>
+      
+      <DashboardMain />
     </div>
   );
 }
