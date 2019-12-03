0006ea3c92a32aaaf1ae4d335e6af4a4e3f36f7f
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-03 12:46:59 +0100

    remove view button from user transaction history

--- src/components/client/Transactions.js
+++ src/components/client/Transactions.js
@@ -6,7 +6,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Button from '@material-ui/core/Button';
+
 import Title from './Title';
 
 // Generate Order Data
