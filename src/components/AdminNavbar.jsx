06157ed088ab4ea14798ac7ca99118525b184cd8
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-06 12:04:21 +0100

    separate adminNavbar into a separate component

---
+++ src/components/AdminNavbar.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import clsx from 'clsx';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Badge from '@material-ui/core/Badge';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Title from './Title';
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    toolbar: {
+      paddingRight: 24, // keep right padding when drawer closed
+    },
+  
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    menuButtonHidden: {
+      display: 'none',
+    },
+
+
+    appBarSpacer: theme.mixins.toolbar,
+  
+  }));
+  
+  
+  const AdminNavbar = (props) => {
+    const classes = useStyles();
+    const {handleDrawerOpen, open, handleProfileMenuOpen} = props;
+      return (
+        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Title>Dashboard</Title>
+          <IconButton color="inherit">
+            <Badge badgeContent={30} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+            edge="end"
+            aria-label="account of current user"
+            // aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      
+
+      )
+  }
+  
+  export default AdminNavbar
+  
+
