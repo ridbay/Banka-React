3823e7b4ba64446346ddd0abcac9051ac3fab421
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-14 07:04:16 +0100

    Updating profile

--- src/components/customer/Profile.jsx
+++ src/components/customer/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
+
 import { CUSTOMER } from '../../Data/userData';
 
 
@@ -11,9 +11,11 @@ import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
@@ -26,7 +28,8 @@ const useStyles = makeStyles(theme => ({
         
     },
     media: {
-        height: 240,
+        height: 0,
+    paddingTop: '56.25%', // 16:9
     },
     content: {
         flexGrow: 1,
@@ -52,8 +55,6 @@ export default function Profile() {
     const classes = useStyles();
 
     const { role, fname, lname, image, phone, accountNumber,email } = CUSTOMER[1];
-    console.log(Image)
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
     return (
         <div className={classes.root}>
@@ -64,35 +65,35 @@ export default function Profile() {
                     <Grid container justify="center" spacing={3}>
 
                         <Grid item xs={12} sm={12} md={8} lg={9}>
-                            
-                            <Card className={classes.card}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        className={classes.media}
-                                        image={image}
-                                        title={`${fname}'s picture`}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {fname} {lname} 
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p"> 
-                                        {role}
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p"> 
-                                        Email: {email}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                <Typography variant="body2" color="textSecondary" component="p"> 
-                                        Account Number: {accountNumber}
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p"> 
-                                        Phone Number: {phone}
-                                        </Typography>
-                                </CardActions>
-                            </Card>
+                        <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="profile" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        
+        title={fname}
+        subheader={role}
+      />
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title={`${fname}'s picture`}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+      <Typography variant="body2" color="textSecondary" component="p">
+          Something can be here
+        </Typography>
+      </CardActions>
+      
+    </Card>
                         </Grid>
 
                         {/* <Grid item xs={12} sm={12} md={4} lg={3}>
