52f8d8da6d0492cc87a1ece535d45e07224af0b8
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-28 18:00:34 +0100

    Fix select option field

--- src/components/admin/CreateAdminStaffAccountButton.jsx
+++ src/components/admin/CreateAdminStaffAccountButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -8,8 +9,33 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Select from '@material-ui/core/Select';
 
+
+const useStyles = makeStyles(theme => ({
+  selectField: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
 const CreateAdminStaffAccountButton = () => {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+
+  const [state, setState] = React.useState({
+    fName: '',
+    lName: '',
+    email: '',
+    acctType: '',
+    password: '',
+  });
+  
+
+const handleChange = name => event => {
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,8 +58,8 @@ const CreateAdminStaffAccountButton = () => {
             occasionally.
           </DialogContentText>
           <TextField
-            autoFocus
-            margin="dense"
+            // autoFocus
+            // margin="dense"
             id="fName"
             label="First Name"
             type="text"
@@ -58,16 +84,18 @@ const CreateAdminStaffAccountButton = () => {
 
           <Select
             native
-            value='savings'
-            // fullWidth
-            labelWidth={20}
-            inputProps={{
-              name: 'acctType',
-              id: 'acctType',
-            }}
+            value={state.acctType}
+            className={classes.selectField}
+            
+            // labelWidth={20}
+            onChange={handleChange('acctType')}
+            // inputProps={{
+            //   name: 'acctType',
+            //   id: 'acctType',
+            // }}
 
           >
-
+            <option value="">Account Type</option> 
             <option value={"savings"}>Staff</option>
             <option value={"staff"}>Admin</option>
 
