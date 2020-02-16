88069e9c4c8bf15093d2b3231ef5162cd4de16f9
Ridwan Balogun <balogunridwan@gmail.com>
2020-02-16 12:52:49 +0100

    Made some changes

--- src/pages/customer/switchRoutes.js
+++ src/pages/customer/switchRoutes.js
@@ -4,25 +4,21 @@ import routes from '../../routes';
 import { Switch, Route, Redirect } from "react-router-dom";
 
 
-const SwitchRoutes = ()=> {
+const SwitchRoutes = () => (
+  <Switch>
+    {routes.map((route, key) => {
+      if (route.layout === "/customer") {
+        return (
+          <Route
+            path={route.layout + route.path}
+            component={route.component}
+            key={key}
+          />
+        );
+      }
+      return null;
+    })}
+    <Redirect from="/customer" to="/customer/dashboard" />
+  </Switch>);
 
-  
-    return (
-    <Switch>
-      {routes.map((route, key) => {
-        if (route.layout === "/customer") {
-          return (
-            <Route
-              path={route.layout + route.path}
-              component={route.component}
-              key={key}
-            />
-          );
-        }
-        return null;
-      })}
-      <Redirect from="/customer" to="/customer/dashboard" />
-    </Switch>)
-};
-
-  export default SwitchRoutes
+export default SwitchRoutes
